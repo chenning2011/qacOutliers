3b0b982ffece5581a7a8db7f357729e4423489b0
Caleb Henning <chenning@wesleyan.edu>
2024-11-14 14:09:09 -0500

    adding mtcarsOutliers dataset

---
+++ R/mtcarsOutliers.R
@@ -0,0 +1,21 @@
+#' mtcars dataset from base R with outliers
+#'
+#' Modified version of mtcars with generated outliers.
+#'
+#' @format A data frame with 32 rows and 11 variables:
+#' \describe{
+#'   \item{\code{mpg}}{double. DESCRIPTION.}
+#'   \item{\code{cyl}}{double. DESCRIPTION.}
+#'   \item{\code{disp}}{double. DESCRIPTION.}
+#'   \item{\code{hp}}{double. DESCRIPTION.}
+#'   \item{\code{drat}}{double. DESCRIPTION.}
+#'   \item{\code{wt}}{double. DESCRIPTION.}
+#'   \item{\code{qsec}}{double. DESCRIPTION.}
+#'   \item{\code{vs}}{double. DESCRIPTION.}
+#'   \item{\code{am}}{double. DESCRIPTION.}
+#'   \item{\code{gear}}{double. DESCRIPTION.}
+#'   \item{\code{carb}}{double. DESCRIPTION.}
+#' }
+#'
+#' @source mtcars dataset comes from base R, and the outliers were generated using the `generateOutliers` function in the outForest package.
+"mtcarsOutliers"
