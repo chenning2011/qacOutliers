fd86ea49678c321d2ab547e9a1d70632c7afe373
Caleb Henning <chenning@wesleyan.edu>
2024-11-14 14:12:38 -0500

    updating description for mtcarsOutliers dataset

--- R/mtcarsOutliers.R
+++ R/mtcarsOutliers.R
@@ -4,17 +4,17 @@
 #'
 #' @format A data frame with 32 rows and 11 variables:
 #' \describe{
-#'   \item{\code{mpg}}{double. DESCRIPTION.}
-#'   \item{\code{cyl}}{double. DESCRIPTION.}
-#'   \item{\code{disp}}{double. DESCRIPTION.}
-#'   \item{\code{hp}}{double. DESCRIPTION.}
-#'   \item{\code{drat}}{double. DESCRIPTION.}
-#'   \item{\code{wt}}{double. DESCRIPTION.}
-#'   \item{\code{qsec}}{double. DESCRIPTION.}
-#'   \item{\code{vs}}{double. DESCRIPTION.}
-#'   \item{\code{am}}{double. DESCRIPTION.}
-#'   \item{\code{gear}}{double. DESCRIPTION.}
-#'   \item{\code{carb}}{double. DESCRIPTION.}
+#'   \item{\code{mpg}}{double. miles per gallon}
+#'   \item{\code{cyl}}{double. number of cylinders}
+#'   \item{\code{disp}}{double. displacement (cu.in.)}
+#'   \item{\code{hp}}{double. gross horsepower}
+#'   \item{\code{drat}}{double. rear axle ratio}
+#'   \item{\code{wt}}{double. weight (1000 lbs)}
+#'   \item{\code{qsec}}{double. 1/4 mile time}
+#'   \item{\code{vs}}{double. engine (0 = v-shaped, 1 = straight)}
+#'   \item{\code{am}}{double. transmission (0 = automatic, 1 = manual)}
+#'   \item{\code{gear}}{double. number of forward gears}
+#'   \item{\code{carb}}{double. number of carburetors}
 #' }
 #'
 #' @source mtcars dataset comes from base R, and the outliers were generated using the `generateOutliers` function in the outForest package.
